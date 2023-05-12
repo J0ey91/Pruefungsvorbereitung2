a30ebb2464064cccc760f12e999240b34d233d66
Joey-M. Burgdorf <129076185+J0ey91@users.noreply.github.com>
2023-05-12 10:55:47 +0200

    ad

---
+++ src/main/kotlin/DataClass.kt
@@ -0,0 +1,40 @@
+import com.sun.source.tree.Tree
+
+fun main() {
+
+    val hannover = Stadt("Hannover")
+    val tankstelle = Tankstelle("Allertal")
+    val hildesheim = Stadt("Hildesheim")
+
+    hannover.nextOrt.add(tankstelle)
+    tankstelle.nextOrt.add(hildesheim)
+    hildesheim.nextOrt.add(tankstelle)
+    tankstelle.nextOrt.add(hannover)
+
+}
+
+abstract class Ort (open val name : String, open var nextOrt : MutableList<Ort> = mutableListOf()) {
+
+}
+
+data class Stadt(override val name: String, override var nextOrt: MutableList<Ort> = mutableListOf()) : Ort(name, nextOrt)  {
+
+}
+
+data class Tankstelle(override val name: String, override var nextOrt: MutableList<Ort> = mutableListOf()) : Ort(name, nextOrt){
+
+}
+
+fun checkTankStop (ort : Ort) : Boolean {
+    TODO()
+}
+
+
+
+data class BinarySearchTree(var root : TreeNode? = null) {
+
+}
+
+data class TreeNode (val value : Int, var left : TreeNode? = null, var right:TreeNode? = null){
+
+}
